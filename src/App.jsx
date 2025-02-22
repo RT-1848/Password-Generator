fdd0a6f145f891ee28cd387caa1a37118b5c43a7
Ryan T <150309976+RT-1848@users.noreply.github.com>
2025-02-22 16:02:46 -0600

    added functionality to the generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { FaClipboard } from "react-icons/fa";
+import { useForm } from './useForm';
+import { getRandomChar, getSpecialChar } from './utils';
+import { toast } from "react-hot-toast";
 
 function App() {
+
+    const [values, setValues] = useForm({
+        length: 6,
+        capital: true,
+        small: true,
+        number: false,
+        symbol: false,
+    });
+
+    const [result, setResult] = useState('');
+
+    const fieldsArray = [
+        {
+            field: values.capital,
+            getChar: () => getRandomChar(65, 90),
+        },
+        {
+            field: values.small,
+            getChar: () => getRandomChar(97, 122),
+        },
+        {
+            field: values.number,
+            getChar: () => getRandomChar(48, 57),
+        },
+        {
+            field: values.symbol,
+            getChar: () => getSpecialChar(),
+        },
+    ];
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        let generatedPassword = "";
+        const checkedFields = fieldsArray.filter(({ field }) => field );
+
+        for (let i = 0; i < values.length; i++) {
+            const index = Math.floor(Math.random() * checkedFields.length);
+            const letter = checkedFields[index]?.getChar();
+
+            if (letter) {
+                generatedPassword += letter;
+            }
+
+        }
+        if (generatedPassword) {
+            setResult(generatedPassword);
+        }
+        else {
+            toast.error("Please select an option");
+        }
+    }
+
+    const handleClipboard = async() => {
+        if (result) {
+            await navigator.clipboard.writeText(result);
+            toast.success("Copied to your clipboard");
+        }
+        else {
+            toast.error("No password to copy");
+        }
+    }
+
     return (
         <section>
             <div className="container">
-                <form id="pg-form">
+                <form id="pg-form" onSubmit={handleOnSubmit}>
                     <div className="result">
-                        <input type="text" id="result" placeholder="minimum 6 characters" readOnly></input>
-                        <div className="clipboard"> 
+                        <input type="text" id="result" placeholder="minimum 6 characters" readOnly value={result}></input>
+                        <div className="clipboard" onClick={handleClipboard}> 
                             <FaClipboard></FaClipboard>
                         </div>
                     </div>
                     <div>
                         <div className="field">
                             <label htmlFor="length">Length</label>
-                            <input type="number" id="length" min={6} max={20}></input>
+                            <input type="number" id="length" min={6} max={20} name="length" value={values.length} onChange={setValues}></input>
                         </div>
                         <div className="field">
                             <label htmlFor="capital">Uppercase Letters</label>
-                            <input type="checkbox" id="capital"></input>
+                            <input type="checkbox" id="capital" name="capital" checked={values.capital} onChange={setValues}></input>
                         </div>
                         <div className="field">
                             <label htmlFor="small">Lowercase Letters</label>
-                            <input type="checkbox" id="small"></input>
+                            <input type="checkbox" id="small" name="small" checked={values.small} onChange={setValues}></input>
                         </div>
                         <div className="field">
                             <label htmlFor="number">Numbers</label>
-                            <input type="checkbox" id="number"></input>
+                            <input type="checkbox" id="number" name="number" checked={values.number} onChange={setValues}></input>
                         </div>
                         <div className="field">
                             <label htmlFor="symbol">Symbols</label>
-                            <input type="checkbox" id="symbol"></input>
+                            <input type="checkbox" id="symbol" name="symbol" checked={values.symbol} onChange={setValues}></input>
                         </div>
                     </div>
                     <button type="submit">Generate Password</button>
