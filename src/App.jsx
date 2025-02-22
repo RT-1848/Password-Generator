3c0e77864321f458d643313c67abef430526dce0
Ryan T <150309976+RT-1848@users.noreply.github.com>
2025-02-21 19:25:30 -0600

    Implemented Styling for the Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { FaClipboard } from "react-icons/fa";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <section>
+            <div className="container">
+                <form id="pg-form">
+                    <div className="result">
+                        <input type="text" id="result" placeholder="minimum 6 characters" readOnly></input>
+                        <div className="clipboard"> 
+                            <FaClipboard></FaClipboard>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="field">
+                            <label htmlFor="length">Length</label>
+                            <input type="number" id="length" min={6} max={15}></input>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="capital">Uppercase Letters</label>
+                            <input type="checkbox" id="capital"></input>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="small">Lowercase Letters</label>
+                            <input type="checkbox" id="small"></input>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="number">Numbers</label>
+                            <input type="checkbox" id="number"></input>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="symbol">Symbols</label>
+                            <input type="checkbox" id="symbol"></input>
+                        </div>
+                    </div>
+                    <button type="submit">Generate Password</button>
+                </form>
+            </div>
+        </section>
+    )
 }
 
 export default App
